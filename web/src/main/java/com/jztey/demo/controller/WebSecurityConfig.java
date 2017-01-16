d932df282ba15892b1b27ce574c6e285dbd3a595
yushi <yushi@DESKTOP-FU7CKIG>
2017-01-16 09:02:41 +0800

    增加一下注释，安全拦截是拦截相应的bean，不是所有的bean

--- web/src/main/java/com/jztey/demo/controller/WebSecurityConfig.java
+++ web/src/main/java/com/jztey/demo/controller/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.jztey.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,26 +9,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.jztey.demo.service.CustomUserService;
 
-@Configuration // 过滤器的安全认证生效
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {// 1
 
 	@Bean
-	UserDetailsService customUserService() { // 2// 注册CustomUserService的Bean
+	UserDetailsService customUserService() { // 2//
+												// 注册CustomUserService的Bean,需要进行拦截的bean
 		return new CustomUserService();
 	}
-
-	
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(customUserService()); // 3// 添加我们自定义的user detail
 		// service认证
-
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated() // 所有请求需要认证即登陆才能访问
+		http.authorizeRequests().anyRequest().authenticated() // 所有请求需要认证即登陆才能访问，只是对上面进行bean进行拦截，其他的不用拦截
 				.and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll() // 5定制登陆行为，登陆页面可以任意访问
 				.and().logout().permitAll(); // 6定制注销的行为，注销请求可以任意访问
 
--- web/src/main/java/com/jztey/demo/service/UserServiceImpl.java
+++ web/src/main/java/com/jztey/demo/service/UserServiceImpl.java
@@ -21,6 +21,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
@@ -331,4 +334,6 @@ public class UserServiceImpl extends BaseService<User> implements UserService {
 		return healthAccount + "";
 	}
 
+	
+ 
 }
